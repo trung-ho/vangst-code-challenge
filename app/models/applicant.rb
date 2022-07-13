5bf38604cd9ad1d91df4da498074e30514a3f672
Michael Ho <chacharoti@gmail.com>
2022-07-13 01:10:43 -0400

    Add validation to prevent exceed number of workers needed

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -3,4 +3,13 @@ class Applicant < ActiveRecord::Base
 
   validates :name, presence: true, allow_nil: false
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validate :check_number_of_candidates, on: :create
+
+  private
+
+  def check_number_of_candidates
+    if job_posting && job_posting.applicants.count == job_posting.number_of_workers
+      errors.add(:job_posting, 'is full of candidates') 
+    end
+  end
 end
--- spec/models/applicants_spec.rb
+++ spec/models/applicants_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Applicant, type: :model do
     @job_posting = JobPosting.create(
       name: 'Job name',
       description: 'Job description', 
-      number_of_workers: 1 )
+      number_of_workers: 3)
   end
 
   describe "#validate" do
@@ -51,6 +51,21 @@ RSpec.describe Applicant, type: :model do
       expect {
         applicant.save
       }.to change(Applicant, :count).by(1)
+    end
+
+    context "when number of applicants exceed number of workers needed" do
+      it 'return error' do
+        (1..@job_posting.number_of_workers).each do |count|
+          @job_posting.applicants.create name: 'John', email: 'user@example.com'
+        end
+
+        expect(@job_posting.applicants.count).to eq @job_posting.number_of_workers
+        
+        exceed_applicant = @job_posting.applicants.create name: 'John', email: 'user@example.com'
+        expect(
+          exceed_applicant.errors.messages[:job_posting].last
+        ).to eq "is full of candidates"
+      end
     end
   end
 end
